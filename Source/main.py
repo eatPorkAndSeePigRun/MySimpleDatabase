8235a44b918e83aae09b199ee0bc4dcc131c619c
eatPorkAndSeePigRun <811302964@qq.com>
2019-01-25 20:25:20 +0800

    B-Tree leaf node Format

--- Source/main.py
+++ Source/main.py
@@ -1,81 +1,84 @@
 import os
-import sys
 import struct
+from sys import argv
 
 
 class InputBuffer:
-    def __init__(self, buffer=""):
-        self.buffer = buffer
+    def __init__(self, buf):
+        self.buffer = buf
 
 
 # execute result
-EXECUTE_SUCCESS     = 0
-EXECUTE_TABLE_FULL  = 1
+EXECUTE_SUCCESS = 0
+EXECUTE_TABLE_FULL = 1
 
 # meta command result
-META_COMMAND_SUCCESS                = 0
-META_COMMAND_UNRECOGNIZED_COMMAND   = 1
+META_COMMAND_SUCCESS = 0
+META_COMMAND_UNRECOGNIZED_COMMAND = 1
 
 # prepare result
-PREPARE_SUCCESS                 = 0
-PREPARE_SYNTAX_ERROR            = 1 
-PREPARE_UNRECOGNIZED_SUCCESS    = 2
-PREPARE_STRING_TOO_LONG         = 3
-PREPARE_NEGATIVE_ID             = 4
+PREPARE_SUCCESS = 0
+PREPARE_SYNTAX_ERROR = 1
+PREPARE_UNRECOGNIZED_SUCCESS = 2
+PREPARE_STRING_TOO_LONG = 3
+PREPARE_NEGATIVE_ID = 4
 
 # statement type
-STATEMENT_INSERT    = 0
-STATEMENT_SELECT    = 1
+STATEMENT_INSERT = 0
+STATEMENT_SELECT = 1
 
-COLUMN_USERNAME_SIZE    = 32
-COLUMN_EMAIL_SIZE       = 255
+COLUMN_USERNAME_SIZE = 32
+COLUMN_EMAIL_SIZE = 255
 
 
 class Row:
-    def __init__(self, id=0, username="", email=""):
+    def __init__(self, id, username, email):
         self.id = id
         self.username = username
         self.email = email
 
 
 class Statement:
-    def __init__(self, type=0, row_to_insert=Row()):
-        self.type = type
-        self.row_to_insert = row_to_insert
+    def __init__(self, _type):
+        self.type = _type
+        self.row_to_insert = None
 
 
 # compact representation of a row
-ID_SIZE         = struct.calcsize("i")
-USERNAME_SIZE   = struct.calcsize("%ds" % COLUMN_USERNAME_SIZE)
-EMAIL_SIZE      = struct.calcsize("%ds" % COLUMN_EMAIL_SIZE)
-ID_OFFSET       = 0
+ID_SIZE = struct.calcsize("I")
+USERNAME_SIZE = struct.calcsize("%ds" % COLUMN_USERNAME_SIZE)
+EMAIL_SIZE = struct.calcsize("%ds" % COLUMN_EMAIL_SIZE)
+ID_OFFSET = 0
 USERNAME_OFFSET = ID_OFFSET + ID_SIZE
-EMAIL_OFFSET    = USERNAME_OFFSET + USERNAME_SIZE
-ROW_SIZE        = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE
+EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE
+ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE
 
-PAGE_SIZE       = 4096
+PAGE_SIZE = 4096
 TABLE_MAX_PAGES = 100
-ROWS_PER_PAGE   = PAGE_SIZE / ROW_SIZE
-TABLE_MAX_ROWS  = ROWS_PER_PAGE * TABLE_MAX_PAGES
+
+malloc_a_page_memory = lambda: "\x00" * PAGE_SIZE
+modify_memory = lambda page, buf, offset: page[:offset] + buf + page[(offset + len(buf)):]
 
 
 class Pager:
-    def __init__(self, file_descriptor=-1, file_length=-1, pages=["" for i in range(TABLE_MAX_PAGES)]):
+    def __init__(self, file_descriptor, file_length, num_pages):
         self.file_descriptor = file_descriptor
         self.file_length = file_length
-        self.pages = pages
+        self.num_pages = num_pages
+        self.pages = ["" for _ in range(TABLE_MAX_PAGES)]
 
 
 class Table:
-    def __init__(self, pager=Pager(), num_rows=0):
+    def __init__(self, pager, root_page_num):
         self.pager = pager
-        self.num_rows = num_rows
+        self.root_page_num = root_page_num
 
 
 class Cursor:
-    def __init__(self, table=Table(), row_num=0, end_of_table=False):
+    def __init__(self, table, page_num, cell_num, end_of_table):
         self.table = table
-        self.row_num = row_num
+        self.page_num = page_num
+        self.cell_num = cell_num
         self.end_of_table = end_of_table
 
 
@@ -86,35 +89,98 @@ def print_row(row):
     print "(%d, %s, %s)" % (id, username, email)
 
 
-def serialize_row(src, dest):
-    temp = struct.pack("i", src.id)
-    temp = temp + struct.pack("%ds" % USERNAME_SIZE, src.username)
-    temp = temp + struct.pack("%ds" % EMAIL_SIZE, src.email)
+# node type
+NODE_INTERNAL = 0
+NODE_LEAF = 1
 
-    pages, page_num, byte_offset = dest
-    old_page = pages[page_num]
-    new_page = old_page[:byte_offset] + temp + old_page[byte_offset+len(temp):]
-    pages[page_num] = new_page    
+# common node header layout
+NODE_TYPE_SIZE = struct.calcsize("B")
+NODE_TYPE_OFFSET = 0
+IS_ROOT_SIZE = struct.calcsize("B")
+IS_ROOT_OFFSET = NODE_TYPE_SIZE
+PARENT_POINTER_SIZE = struct.calcsize("I")
+PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE
+COMMON_NODE_HEADER_SIZE = NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE
+
+# leaf node header layout
+LEAF_NODE_NUM_CELLS_SIZE = struct.calcsize("I")
+LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE
+LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE
+
+# leaf node body layout
+LEAF_NODE_KEY_SIZE = struct.calcsize("I")
+LEAF_NODE_KEY_OFFSET = 0
+LEAF_NODE_VALUE_SIZE = ROW_SIZE
+LEAF_NODE_VALUE_OFFSET = LEAF_NODE_KEY_OFFSET + LEAF_NODE_KEY_SIZE
+LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE
+LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE
+LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE
 
 
-def deserialize_row(src, dest):
-    pages, page_num, byte_offset = src
-    page = pages[page_num]
+def leaf_node_num_cells(node):
+    offset = LEAF_NODE_NUM_CELLS_OFFSET
+    string = node[offset: offset + LEAF_NODE_NUM_CELLS_SIZE]
+    return struct.unpack("I", string)[0]
 
-    temp = page[byte_offset:]
-    dest.id = struct.unpack("i", temp[ID_OFFSET : ID_OFFSET+ID_SIZE])[0]
-    dest.username = struct.unpack("%ds" % USERNAME_SIZE, temp[USERNAME_OFFSET : USERNAME_OFFSET+USERNAME_SIZE])[0]
-    dest.email = struct.unpack("%ds" % EMAIL_SIZE, temp[EMAIL_OFFSET : EMAIL_OFFSET+EMAIL_SIZE])[0]
+
+def leaf_node_cell(node, cell_num):
+    offset = LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE
+    return node[offset: offset + LEAF_NODE_NUM_CELLS_SIZE]
+
+
+def leaf_node_key(node, cell_num):
+    buf = leaf_node_cell(node, cell_num)
+    return struct.unpack("I", buf)[0]
+
+
+def leaf_node_value(node, cell_num):
+    offset = (LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE) + LEAF_NODE_KEY_SIZE
+    return node[offset: offset + LEAF_NODE_VALUE_SIZE]
+
+
+def print_constants():
+    print "ROW_SIZE: %d" % ROW_SIZE
+    print "COMMON_NODE_HEADER_SIZE: %d" % COMMON_NODE_HEADER_SIZE
+    print "LEAF_NODE_HEADER_SIZE: %d" % LEAF_NODE_HEADER_SIZE
+    print "LEAF_NODE_CELL_SIZE: %d" % LEAF_NODE_CELL_SIZE
+    print "LEAF_NODE_SPACE_FOR_CELLS: %d" % LEAF_NODE_SPACE_FOR_CELLS
+    print "LEAF_NODE_MAX_CELLS: %d" % LEAF_NODE_MAX_CELLS
+
+
+def print_leaf_node(node):
+    num_cells = leaf_node_num_cells(node)
+    print "leaf (size %d)" % num_cells
+    for i in range(num_cells):
+        key = leaf_node_key(node, i)
+        print "  - %d : %d" % (i, key)
+
+
+def serialize_row(src):
+    dest = struct.pack("I", src.id)
+    dest = dest + struct.pack("%ds" % USERNAME_SIZE, src.username)
+    dest = dest + struct.pack("%ds" % EMAIL_SIZE, src.email)
+    return dest
+
+
+def deserialize_row(src):
+    id = struct.unpack("I", src[ID_OFFSET: ID_OFFSET + ID_SIZE])[0]
+    username = struct.unpack("%ds" % USERNAME_SIZE, src[USERNAME_OFFSET: USERNAME_OFFSET + USERNAME_SIZE])[0]
+    email = struct.unpack("%ds" % EMAIL_SIZE, src[EMAIL_OFFSET: EMAIL_OFFSET + EMAIL_SIZE])[0]
+    return Row(id, username, email)
+
+
+def initialize_leaf_node(node):
+    buf = struct.pack("I", 0)
+    return modify_memory(node, buf, LEAF_NODE_NUM_CELLS_OFFSET)
 
 
 def get_page(pager, page_num):
-    malloc_memory = lambda : "\x00" * PAGE_SIZE
-
     if page_num > TABLE_MAX_PAGES:
         print "Tried to fetch page number out of bounds. %d > %d" % (page_num, TABLE_MAX_PAGES)
         exit(0)
+
     if not pager.pages[page_num]:
-        page = malloc_memory()
+        page = malloc_a_page_memory()
         num_pages = pager.file_length / PAGE_SIZE
         if pager.file_length % PAGE_SIZE:
             num_pages += 1
@@ -122,31 +188,38 @@ def get_page(pager, page_num):
             pager.file_descriptor.seek(page_num * PAGE_SIZE, os.SEEK_SET)
             buf = pager.file_descriptor.read(PAGE_SIZE)
             if buf:
-                page = buf + page[len(buf):]
+                page = buf
         pager.pages[page_num] = page
+        if page_num >= pager.num_pages:
+            pager.num_pages = page_num + 1
 
+    return pager.pages[page_num]
 
 
 def table_start(table):
-    return Cursor(table, 0, table.num_rows == 0)
+    root_node = get_page(table.pager, table.root_page_num)
+    num_cells = leaf_node_num_cells(root_node)
+    return Cursor(table, table.root_page_num, 0, num_cells == 0)
 
 
 def table_end(table):
-    return Cursor(table, table.num_rows, True)
+    root_node = get_page(table.pager, table.root_page_num)
+    num_cells = leaf_node_num_cells(root_node)
+    return Cursor(table, table.root_page_num, num_cells, True)
 
 
 def cursor_value(cursor):
-    row_num = cursor.row_num
-    page_num = row_num / ROWS_PER_PAGE
-    get_page(cursor.table.pager, page_num)
-    row_offset = row_num % ROWS_PER_PAGE
-    byte_offset = row_offset * ROW_SIZE
-    return cursor.table.pager.pages, page_num, byte_offset
+    page_num = cursor.page_num
+    page = get_page(cursor.table.pager, page_num)
+    return leaf_node_value(page, cursor.cell_num)
 
 
 def cursor_advance(cursor):
-    cursor.row_num += 1
-    if cursor.row_num >= cursor.table.num_rows:
+    page_num = cursor.page_num
+    node = get_page(cursor.table.pager, page_num)
+
+    cursor.cell_num += 1
+    if cursor.cell_num >= leaf_node_num_cells(node):
         cursor.end_of_table = True
 
 
@@ -154,14 +227,24 @@ def pager_open(filename):
     fd = open(filename, "rb+")
     fd.seek(0, os.SEEK_END)
     file_length = fd.tell()
-    pager = Pager(fd, file_length)
-    return pager 
+    if file_length % PAGE_SIZE != 0:
+        print "Db file is not a whole number of pages. Corrupt file."
+        exit(0)
+    num_pages = file_length / PAGE_SIZE
+
+    pager = Pager(fd, file_length, num_pages)
+    return pager
 
 
 def db_open(filename):
     pager = pager_open(filename)
-    num_rows = pager.file_length / ROW_SIZE
-    table = Table(pager, num_rows)
+    table = Table(pager, 0)
+
+    if pager.num_pages == 0:
+        # New database file. Initialize page 0 as leaf node.
+        root_node = get_page(pager, 0)
+        table.pager.pages[0] = initialize_leaf_node(root_node)
+
     return table
 
 
@@ -174,29 +257,22 @@ def read_input():
     return InputBuffer(buf)
 
 
-def pager_flush(pager, page_num, size):
+def pager_flush(pager, page_num):
     if not pager.pages[page_num]:
         print "Tried to flush null page"
         exit(0)
 
     pager.file_descriptor.seek(page_num * PAGE_SIZE, os.SEEK_SET)
-    pager.file_descriptor.write(pager.pages[page_num][:size])
+    pager.file_descriptor.write(pager.pages[page_num])
 
 
 def db_close(table):
     pager = table.pager
-    num_full_pages = table.num_rows / ROWS_PER_PAGE
 
-    for i in range(num_full_pages):
+    for i in range(pager.num_pages):
         if not pager.pages[i]:
             continue
-        pager_flush(pager, i, PAGE_SIZE)
-    
-    num_additional_rows = table.num_rows % ROWS_PER_PAGE
-    if num_additional_rows > 0:
-        page_num = num_full_pages
-        if pager.pages[page_num]:
-            pager_flush(pager, page_num, num_additional_rows * ROW_SIZE)
+        pager_flush(pager, i)
 
     pager.file_descriptor.close()
 
@@ -205,6 +281,14 @@ def do_meta_command(input_buffer, table):
     if input_buffer.buffer == ".exit":
         db_close(table)
         exit(1)
+    elif input_buffer.buffer == ".btree":
+        print "Tree:"
+        print_leaf_node(get_page(table.pager, 0))
+        return META_COMMAND_SUCCESS
+    elif input_buffer.buffer == ".constants":
+        print "Constants:"
+        print_constants()
+        return META_COMMAND_SUCCESS
     else:
         return META_COMMAND_UNRECOGNIZED_COMMAND
 
@@ -238,23 +322,52 @@ def prepare_statement(input_buffer):
         return None, PREPARE_UNRECOGNIZED_SUCCESS
 
 
+def leaf_node_insert(cursor, key, value):
+    node = get_page(cursor.table.pager, cursor.page_num)
+
+    num_cells = leaf_node_num_cells(node)
+    if num_cells >= LEAF_NODE_MAX_CELLS:
+        # node full
+        print "Need to implement splitting a leaf node."
+        exit(0)
+
+    if cursor.cell_num < num_cells:
+        # Make room for new cell
+        for i in range(num_cells, cursor.cell_num, -1):
+            offset = LEAF_NODE_HEADER_SIZE + i * LEAF_NODE_CELL_SIZE
+            node = modify_memory(node, leaf_node_cell(node, i - 1), offset)
+
+    # update num cells
+    buf = struct.pack("I", leaf_node_num_cells(node) + 1)
+    node = modify_memory(node, buf, LEAF_NODE_NUM_CELLS_OFFSET)
+    # insert node key
+    buf = struct.pack("I", key)
+    offset = LEAF_NODE_HEADER_SIZE + cursor.cell_num * LEAF_NODE_CELL_SIZE
+    node = modify_memory(node, buf, offset)
+    # insert node value
+    buf = serialize_row(value)
+    offset = (LEAF_NODE_HEADER_SIZE + cursor.cell_num * LEAF_NODE_CELL_SIZE) + LEAF_NODE_KEY_SIZE
+    node = modify_memory(node, buf, offset)
+
+    cursor.table.pager.pages[cursor.page_num] = node
+
+
 def execute_insert(statement, table):
-    if table.num_rows >= TABLE_MAX_PAGES:
+    node = get_page(table.pager, table.root_page_num)
+    if leaf_node_num_cells(node) >= LEAF_NODE_MAX_CELLS:
         return EXECUTE_TABLE_FULL
-    
+
     row_to_insert = statement.row_to_insert
     cursor = table_end(table)
-    serialize_row(row_to_insert, cursor_value(cursor))    
 
-    table.num_rows += 1
+    leaf_node_insert(cursor, row_to_insert.id, row_to_insert)
     return EXECUTE_SUCCESS
 
 
-def execute_select(statement, table):
+def execute_select(table):
     cursor = table_start(table)
-    row = Row()
     while not cursor.end_of_table:
-        deserialize_row(cursor_value(cursor), row)
+        row = deserialize_row(cursor_value(cursor))
         print_row(row)
         cursor_advance(cursor)
 
@@ -265,7 +378,7 @@ def execute_statement(statement, table):
     if statement.type == STATEMENT_INSERT:
         return execute_insert(statement, table)
     elif statement.type == STATEMENT_SELECT:
-        return execute_select(statement, table)
+        return execute_select(table)
 
 
 def main(argv):
@@ -310,4 +423,5 @@ def main(argv):
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main(argv)
+
